53a4854396127e98b3c0c9106dbeafc6e625890f
Michael Jalkio <mjalkio@classy.org>
2017-08-22 21:00:02 -0700

    Remove everything from sepsis-quiz.php

--- twentyseventeen/sepsis-quiz.php
+++ twentyseventeen/sepsis-quiz.php
@@ -16,34 +16,9 @@ wp_head(); ?>
 
 <body>
 <div class="main-container">
-    <div id="questions_container" class="question-container">
-        <!-- QUESTION -->
-        <div class="question-container">
-            <div class="question-number">Question</div>
-            <div class="under-card-top under-card-top-hide"></div>
+    <div id="questions_container" class="question-container"></div>
 
-            <!-- MAIN CARD -->
-            <div class="card-container">
-                  <div class="question">Question</div>
-                  <div class="choices">
-                  </div>
-            </div>
-
-            <!-- BOTTOM CARD -->
-            <div class="under-card-bottom-container">
-                <div class="under-card-bottom under-card-bottom-reveal">
-                    <div>Fact</div>
-                    <div class="learn-more"><a href="" target="_blank">Learn More <i class="fa fa-angle-right" aria-hidden="true"></i>
-        </a></div>
-                </div>
-            </div>
-        </div>
-        <!-- END OF QUESTION -->
-    </div>
-
-    <!-- SHARE -->
     <div id="share_container" class="share-container"></div>
-    <!-- END OF SHARE -->
 </div>
 
 
